9ef7ffb588f16fcc4bc44e547f789bc2c52ea559
sleepinducer <sammysainz5@gmail.com>
2025-07-23 01:39:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 const newPara = document.createElement("p");
 newPara.textContent = "This paragraph was created by JavaScript!";
 document.body.appendChild(newPara);
+
+const newHeader = document.createElement('h1');
+newHeader.textContent = `Trailside Junk`;
+document.body.appendChild(newHeader);
+                                    
