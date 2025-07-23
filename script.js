b0513affa71d0145ef31b68796a5abf093ceccab
sleepinducer <sammysainz5@gmail.com>
2025-07-23 01:35:50 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// This will create an HTML element from JavaScript
+const newPara = document.createElement("p");
+newPara.textContent = "This paragraph was created by JavaScript!";
+document.body.appendChild(newPara);
