5021832709aa9818bfdc76d72b9249612ce9b4dc
sleepinducer <sammysainz5@gmail.com>
2025-07-23 14:48:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,16 @@
 const newPara = document.createElement("p");
 newPara.textContent = "This paragraph was created by JavaScript!";
 document.body.appendChild(newPara);
+newPara.classList.add("newPara");
 
 const newHeader = document.createElement('h1');
 newHeader.textContent = `Trailside Junk`;
 document.body.appendChild(newHeader);
+newHeader.classList.add("newHeader");
 
 const secondHeader = document.createElement('h1');
 secondHeader.textContent  = `Serving Charlotte and Surrounding since 1983`;
 document.body.appendChild(secondHeader);
+secondHeader.classList.add("secondHeader");
 
                                     
